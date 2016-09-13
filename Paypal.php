1effa6efe07ab84e23ec9b481ea26647a6e05be5
Pierre Rudloff <contact@rudloff.pro>
2016-09-13 23:23:31 +0200

    Use Archi-Strasbourg Paypal form

--- Paypal.php
+++ Paypal.php
@@ -22,11 +22,11 @@ function wfPayPalExtension() {
 
    $ipbwiki_paypal = array();
    # CHANGE THE LINES BELOW TO REFLECT TO YOUR PAYPAL BUTTONS!!! (there's no limit on the number of buttons you define)
-   $ipbwiki_paypal[1] = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="FSV3SUVDJVJE2">
-<input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !">
-<img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+   $ipbwiki_paypal[1] = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input name="cmd" value="_s-xclick" type="hidden">
+<input name="hosted_button_id" value="46Z9GLNM3ATXL" type="hidden">
+<input src="https://www.paypalobjects.com/fr_FR/i/btn/btn_donate_LG.gif" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !" type="image">
+<img alt="" src="https://www.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
 </form>';
 
 }
